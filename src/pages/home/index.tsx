2a20f3cd5265f9c76538811147c15395a0636060
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-06-10 23:51:14 +0900

    header作成

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header } from '../../components/header';
+import "./style.css"
+
+export const Home = () => {
+  return (
+    <div className='main'>
+      <Header></Header>
+    </div>
+  );
+};
